72915dce0bd5d4acd96038d1b7740f7cea32a1fa
Alpharius61 <91472214+Alpharius61@users.noreply.github.com>
2022-02-16 08:49:06 +0100

    up

--- P3 Django/bibliotheca_mobilis/bibliotheca_mobilis/settings.py
+++ P3 Django/bibliotheca_mobilis/bibliotheca_mobilis/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bibliotheca_mobilis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projet3',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Zarole28',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
--- P3 Django/bibliotheca_mobilis/bibliotheca_mobilis/urls.py
+++ P3 Django/bibliotheca_mobilis/bibliotheca_mobilis/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from registration import views as r_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('signup/', r_views.signup, name='signup')
 ]
