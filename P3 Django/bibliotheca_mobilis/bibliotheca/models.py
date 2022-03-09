673aac324424c830e6a391471a33c97b57a2f99c
Alpharius61 <monlouis.gael@gmail.com>
2022-03-09 21:57:20 +0100

    BDD finished

--- P3 Django/bibliotheca_mobilis/bibliotheca/models.py
+++ P3 Django/bibliotheca_mobilis/bibliotheca/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class histoiry (models.Model):
+
+    parents = models.ForeignKey('self', on_delete = models.CASCADE,null=True)
+    article = models.TextField(max_length=20000)
+    title = models.CharField(max_length= 70)
+
+    def __str__(self):
+        return self.title
