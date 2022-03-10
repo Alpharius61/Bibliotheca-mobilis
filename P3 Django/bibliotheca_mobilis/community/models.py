13751702da4dca73c3ad7d84a8108d4795d6ffa8
Alpharius61 <monlouis.gael@gmail.com>
2022-03-10 23:51:47 +0100

    up

--- P3 Django/bibliotheca_mobilis/community/models.py
+++ P3 Django/bibliotheca_mobilis/community/models.py
@@ -1,6 +1,7 @@
+from random import choice
 from django.db import models
 from django.contrib.auth.models import User
-
+from multiselectfield import MultiSelectField
 
 # Create your models here
 
@@ -9,9 +10,8 @@ class chaosAspectVenerated(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-
-class speciality(models.Model):
-    name = models.CharField(max_length=50)
+class specialities(models.Model):
+    name = MultiSelectField(choices = models.CharField(max_length=50))
     def __str__(self):
         return f"{self.name}"
 
@@ -30,7 +30,7 @@ class charactersModel(models.Model):
     historicCreation = models.BooleanField(default = False)
     name = models.CharField(max_length=50)
     biography = models.TextField(max_length=10000)
-    speciality = models.ManyToManyField(speciality)
+    speciality = models.ManyToManyField(specialities)
     side = models.ForeignKey(creationSide,on_delete=models.CASCADE)
     race = models.ForeignKey(creationRace, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -46,7 +46,7 @@ class armyModel(models.Model):
     historicCreation = models.BooleanField(default = False)
     name = models.CharField(max_length=50)
     history = models.TextField(max_length=10000)
-    speciality = models.ManyToManyField(speciality)
+    speciality = models.ManyToManyField(specialities)
     side = models.ForeignKey(creationSide,on_delete=models.CASCADE)
     race = models.ForeignKey(creationRace, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- P3 Django/bibliotheca_mobilis/main/admin.py
+++ P3 Django/bibliotheca_mobilis/main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from community.models import chaosAspectVenerated, creationRace, creationSide, speciality
+from community.models import chaosAspectVenerated, creationRace, creationSide, specialities
 
 # Register your models here.
 admin.site.register(chaosAspectVenerated)
 admin.site.register(creationRace)
 admin.site.register(creationSide)
-admin.site.register(speciality)
+admin.site.register(specialities)
